a9e9f12611cc57f14a751c9058ca5e55734b7720
Alex Windle <alexwindle85@gmail.com>
2016-03-14 16:29:58 -0400

    start javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,52 @@
 // Javascript Entry Point
 
-// import $ from 'jquery';
+import $ from 'jquery';
 
-import users from './users';
+import form_data from './users';
 
-// import { FUNC NAME} from './templates'; 
+import { makeTextInputs } from './templates'; 
+
+// put data to page 
+// users array: direct text, select and textarea objects
+//    to the correct templates
+// run the objects through templates
+// display result to page using .append
+
+form_data.forEach(function(datum) {
+
+	if (form_data.type === "text" || form_data.type === "email" || form_data.type === "tel") {
+		template = makeTextInputs;
+	}
+
+});
+
+// users.forEach(function(user) {
+//   var template;
+//   if (user.type === 'Manager') {
+//     template = manager;
+//   } else if (user.type === 'Employee') {
+//     template = employee;
+//   } else if (user.type === 'Customer') {
+//     template = customer;
+//   }
+//   var html = template(user);
+//   $app.append( html );
+// });
+
+var $app = $('.app');
+
+var userTypeToTemplate = {
+  'Manager': manager,
+  'Employee': employee,
+  'Customer': customer
+}
+
+users.forEach(function(user) {
+  var template = userTypeToTemplate[user.type];
+  var html = template(user);
+  $app.append( html );
+});
+
 
 
 
---
+++ src/js/template.js
@@ -0,0 +1,16 @@
+export function makeTextInputs(input) {
+
+return 
+
+`
+<label for="${input.id}"></label>
+<input type="text" id="${input.id}" placeholder="${input.label}"><br>
+`
+
+};
+
+
+						// `
+						// <label for="first-name"></label>
+						// <input type="text" id="first-name" placeholder="First Name"><br>
+						// `
