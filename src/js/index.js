78b003a889995b1a3735569a66fb28b973036ce5
Alex Windle <alexwindle85@gmail.com>
2016-03-14 20:35:20 -0400

    javascript

--- src/js/index.js
+++ src/js/index.js
@@ -4,59 +4,68 @@ import $ from 'jquery';
 
 import form_data from './users';
 
-import { makeTextInputs, makeLang, makeTextArea } from './templates'; 
+import { makeTextInputs, selectBox, makeTextArea } from './templates'; 
 
 var $app = $('.app');
 
-// put data to page 
-// users array: direct text, select and textarea objects
-//    to the correct templates
-// run the objects through templates
-// display result to page using .append
-
 form_data.forEach(function(datum) {
-
 	var template; 
-
 	if (datum.type === "text" || datum.type === "email" || datum.type === "tel") {
 		template = makeTextInputs;
 	} else if (datum.type === "select") {
-		template = makeLang;
-	} else {
+		template = selectBox;
+	} else if (datum.type === "textarea") {
 		template = makeTextArea;
 	}
 
 	var display = template(datum);
-  	$app.append(display);
+  	$app.append( display );
 
 });
 
-// users.forEach(function(user) {
-//   var template;
-//   if (user.type === 'Manager') {
-//     template = manager;
-//   } else if (user.type === 'Employee') {
-//     template = employee;
-//   } else if (user.type === 'Customer') {
-//     template = customer;
-//   }
-//   var html = template(user);
-//   $app.append( html );
-// });
 
-// var $app = $('.app');
 
-// var userTypeToTemplate = {
-//   'Manager': manager,
-//   'Employee': employee,
-//   'Customer': customer
-// }
 
-// users.forEach(function(user) {
-//   var template = userTypeToTemplate[user.type];
-//   var html = template(user);
-//   $app.append( html );
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/js/templates.js
@@ -0,0 +1,45 @@
+export function makeTextInputs(input) {
+
+return 
+			`
+			<label for="${input.id}"></label>
+			<input type="text" id="${input.id}" placeholder="${input.label}"><br>
+			`
+};
+
+export function selectBox(language){
+
+	var languagesList = language.options;
+
+	var languageHtml = languagesList.map(function(lang) {
+		return 
+		`
+		<label for="${lang.value}"></label>
+		<select id="${lang.value}">
+		<option value="${lang.value}">${lang.label}</Option>
+		`
+	});
+
+export function makeTextArea(input) {
+	return 
+		`
+		<label for="${input.id}"></label>
+		<input type="textarea" id="${input.id}" value="${input.label}"><br>
+		`
+	};
+
+//SCRATCH WORK
+// export function makeLang(lang) {
+
+// return 
+// 			`
+// 			<select id="${lang.id}">
+// 						<option value="${lang.id}">${lang.label}</option>
+// 				  		<option value="EN">English</option>
+// 				  		<option value="FR">French</option>
+// 				  		<option value="SP">Spanish</option>
+// 				  		<option value="CH">Chinese</option>
+// 				  		<option value="JP">Japanese</option>
+// 				  </select><br>
+// 			`
+// };
